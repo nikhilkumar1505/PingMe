2da4580026d1c8a534d888e0f294a88b29162872
nikhil kumar <nklkumar150@gmail.com>
2023-08-14 23:21:57 +0530

    fix: device compaticable warning

--- web/src/routes/ProtectedRoute.tsx
+++ web/src/routes/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import AppRoute from './AppRoute';
 import AuthRoute from './AuthRoute';
 import { useAppSelector } from '../hooks/useAppSelector';
@@ -6,10 +6,11 @@ import { getAvatars, getDetails } from '../store/controllers/User.controller';
 import { useAppDispatch } from '../hooks/useAppDispatch';
 import { getToken } from '../utils/user';
 import { updateLoading, updateLoggedIn } from '../store/slices';
-import { PageLoader } from '../components';
+import { EmptyResult, PageLoader } from '../components';
 import { useNavigate } from 'react-router-dom';
 
 const ProtectedRoute = () => {
+	const [showTabletDiv, setShowTabletDiv] = useState(false);
 	const isSignedIn = useAppSelector((state) => state.app.isLoggedIn);
 	const isLoading = useAppSelector((state) => state.app.isloading);
 	const token = getToken();
@@ -41,6 +42,29 @@ const ProtectedRoute = () => {
 	useEffect(() => {
 		getAvatars();
 	}, []);
+
+	useEffect(() => {
+		function handleResize() {
+			setShowTabletDiv(window.innerWidth < 720);
+		}
+		handleResize();
+		window.addEventListener('resize', handleResize);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
+	if (showTabletDiv) {
+		return (
+			<div style={{ height: '100vh', padding: '2rem' }}>
+				<EmptyResult
+					size='45rem'
+					title='We recommend using a desktop browser for this app. Mobile and tablet compatibility is limited.'
+					imageUrl='https://img.freepik.com/free-vector/hand-drawn-crm-illustration_23-2149398794.jpg?size=626&ext=jpg&ga=GA1.2.304103842.1690276064&semt=ais'
+				/>
+			</div>
+		);
+	}
 
 	if (isLoading) {
 		return <PageLoader />;
